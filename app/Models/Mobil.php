18e4fcd554ebd9aab4f548b5d22ecf17feec4497
Fathin Naufaliya <76151903+fathin123@users.noreply.github.com>
2022-06-22 01:07:58 +0700

    Mobil

---
+++ app/Models/Mobil.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
+
+class Dokter extends Model
+{
+    //use HasFactory;
+    protected $table = "mobils";
+    protected $primaryKey = "id";
+    protected $fillable = [
+        'id', 'nama', 'merk', 'warna', 'tahun'];
+
+    // public function jadwal()
+    // {
+    //     return $this->hasMany(Jadwal::class);
+    // }
+
+    // public function penjadwalan()
+    // {
+    //     return $this->hasMany(Penjadwalan::class);
+    // }
+}
